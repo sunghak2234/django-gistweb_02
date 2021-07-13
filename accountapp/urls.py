7f8ef81a7d4b2bd4ab598dad1e2dd127e2259a9d
sunghak2234 <sunghak2234@gmail.com>
2021-07-13 15:19:30 +0900

    210713 django gistweb_02 (8-1)

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -4,8 +4,6 @@ from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp'
 
-
-
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
     path('create/', AccountCreateView.as_view(), name='create')
--- accountapp/views.py
+++ accountapp/views.py
@@ -2,26 +2,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
-# Create your views here.
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView
-
 from accountapp.models import HelloWorld
-
 
 def hello_world(request):
     if request.method == "POST":
-
         tamp = request.POST.get('hello_world_input')
         new_hello_world = HelloWorld() # 새로운 개체 생성
         new_hello_world.text = tamp
         new_hello_world.save()
-
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
-
     else:
-
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 

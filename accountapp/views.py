82eb3bc321400a304b8a29bbc457ed81ea68413b
sunghak2234 <sunghak2234@gmail.com>
2021-07-13 16:00:04 +0900

    210713 django gistweb_02 (8-2)

--- accountapp/views.py
+++ accountapp/views.py
@@ -21,7 +21,7 @@ def hello_world(request):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:Hello_World')
+    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
 

9790138d984acf126e9587164df343a858270ebd
sunghak2234 <sunghak2234@gmail.com>
2021-08-03 15:55:23 +0900

    210803 django gistweb_02 (17-3)

--- articleapp/models.py
+++ articleapp/models.py
@@ -7,9 +7,7 @@ from django.db import models
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null = True)
-
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True) # media 폴더
     content = models.TextField(null=True)
-
     created_at = models.DateTimeField(auto_now_add=True, null=True)
--- articleapp/views.py
+++ articleapp/views.py
@@ -1,5 +1,5 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
@@ -18,3 +18,13 @@ class ArticleDetailView(DetailView):
     model = Article
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleCreationForm
+    context_object_name = 'target_article'
+    template_name = 'articleapp/update.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detatil', kwargs={'pk':self.object.pk})

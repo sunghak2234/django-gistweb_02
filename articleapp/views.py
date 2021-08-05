2ea4fb0bbf0aea64b0b39f65bc167aacf0f96bc4
sunghak2234 <sunghak2234@gmail.com>
2021-08-05 10:47:29 +0900

    210805 django gistweb_02 (18)

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,18 +1,27 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
+from articleapp.decorators.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
 
+
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ArticleCreateView(CreateView):
     model = Article
     form_class = ArticleCreationForm
-    success_url = reverse_lazy('articleapp:list')
     template_name = 'articleapp/create.html'
+
 
     def form_valid(self, form):
         form.instance.writer = self.request.user
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleapp:detatil', kwargs={'pk': self.object.pk})
 
 class ArticleDetailView(DetailView):
     model = Article
@@ -20,6 +29,8 @@ class ArticleDetailView(DetailView):
     template_name = 'articleapp/detail.html'
 
 
+@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'post')
 class ArticleUpdateView(UpdateView):
     model = Article
     form_class = ArticleCreationForm
@@ -28,3 +39,19 @@ class ArticleUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse('articleapp:detatil', kwargs={'pk':self.object.pk})
+
+
+@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'post')
+class ArticleDeleteView(DeleteView):
+    model = Article
+    context_object_name = 'target_article'
+    success_url = reverse_lazy('articleapp:list')
+    template_name = 'articleapp/delete.html'
+
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list' # 게시글의 리스트를 의미, 여기에 모든 게시글의 게체가 담긴다
+    template_name = 'articleapp/list.html'
+    paginate_by = 20 # 한 페이지에 20개의 게시글

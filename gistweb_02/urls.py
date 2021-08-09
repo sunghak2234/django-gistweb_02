255553cd530a0ee9bb18b7d828059c5b89202cec
sunghak2234 <sunghak2234@gmail.com>
2021-08-09 09:42:23 +0900

    210809 django gistweb_02 (19-2)

--- gistweb_02/urls.py
+++ gistweb_02/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
     path('articles/', include('articleapp.urls')),
+    path('comments/', include('commnetapp.urls'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

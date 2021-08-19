e617126e0efaac633267465e82a063b517764677
sunghak2234 <sunghak2234@gmail.com>
2021-08-19 10:25:38 +0900

    210819 django gistweb_02 (23-3)

--- projectapp/views.py
+++ projectapp/views.py
@@ -5,25 +5,34 @@ from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic.list import MultipleObjectMixin
+
+from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
+
 
 @method_decorator(login_required, 'get')
 @method_decorator(login_required, 'post')
 class ProjectCreateView(CreateView):
     model = Project
     form_class = ProjectCreationForm
-    success_url = reverse_lazy('articleapp:list')
     template_name = 'projectapp/create.html'
 
     def get_success_url(self):
         return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
 
-class ProjectDetailView(DetailView):
+
+class ProjectDetailView(DetailView, MultipleObjectMixin):
     model = Project
-    context_object_name = 'target_projcet'
+    context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
 
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        article_list = Article.objects.filter(project=self.object)
+        return super().get_context_data(object_list=article_list, **kwargs)
 
 class ProjectListView(ListView):
     model = Project

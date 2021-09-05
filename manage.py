224e0ea8f736261f4f0d7dca052fb9e1c53135d1
sunghak2234 <sunghak2234@gmail.com>
2021-09-06 06:22:23 +0900

    210906 django gistweb_02 (a5)

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gistweb_02.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gistweb_02.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
